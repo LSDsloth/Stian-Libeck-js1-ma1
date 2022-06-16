6b626f071e91e4a9c1589d4994f801046c6d12c4
LSDsloth <lst1788@gmail.com>
2022-06-17 00:19:16 +0200

    Done Q 1-7

--- js/script.js
+++ js/script.js
@@ -1,55 +1,68 @@
-
-
 // Question 1
 const cat = {
-    complain: function(){
-        console.log("MJÆOOOOOO")
-    }
-}
+  complain: function () {
+    console.log("MJÆOOOOOO");
+  },
+};
 
 console.log(cat.complain());
-
 
 // Question 2
 const heading = document.querySelector("h3");
 heading.innerHTML = "Updated heading";
 
-
 // Question 3
 heading.style.fontSize = "2em";
 
-
 // Question 4
-// Answer goes here
-
+heading.className = "subHeading";
 
 // Question 5
-const paragraphs = document.querySelector("p");
-console.log(paragraphs.length);
+const paragraphs = document.querySelectorAll("p");
+const paragraphsLength = paragraphs.length;
 
+for (var i = 0; i < paragraphsLength; i++) {
+  paragraphs[i].style.color = "red";
+}
 
 // Question 6
-// Answer goes here
-
+var resultsContainer = document.querySelector(".results");
+resultsContainer.innerHTML = "<p>" + "New paragraph" + "</p>";
+resultsContainer.style.backgroundColor = "yellow";
 
 // Question 7
 function function1(list) {
-    for( var i = 0; i < list.length; i++) {
-        console.log(list[i].name);
-    }
+  for (var i = 0; i < list.length; i++) {
+    console.log(list[i].name);
+  }
 }
 
 const cats = [
-    {
-        name: "Blob",
-        age: 10
-    },
-    {
-        name: "Harold",
-    },
-    {
-        name: "Blurt",
-        age: 21
-    }
+  {
+    name: "Blob",
+    age: 10,
+  },
+  {
+    name: "Harold",
+  },
+  {
+    name: "Blurt",
+    age: 21,
+  },
 ];
- function1(cats)
+function1(cats);
+// const catsLength = cats.length;
+// console.log(catsLength);
+
+// Question 8
+
+function createCats(cats) {
+  for (var i = 0; i < cats.length; i++) {
+    console.log(cats[i]);
+    cats[
+      i
+    ].name.innerHTML = `<div> <h3> ${cats[i].name.innerHTML} </h3> </div>`;
+  }
+}
+
+createCats(cats);
